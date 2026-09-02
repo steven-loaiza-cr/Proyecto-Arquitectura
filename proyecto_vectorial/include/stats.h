807df82dd16ab0ffba25450009ac401bc569045e
steven-loaiza-cr <steven.loaiza.cr@gmail.com>
2026-09-01 22:48:32 -0600

    Comentario para la explicacion del funcionamiento de la funciones auxiliares sum_array, compute_stats, normalize_array con el fin de poder identificar los parametros que necesitamos para los archivos .asm y que no exista corrupcion con el driver.c

--- proyecto_vectorial/include/stats.h
+++ proyecto_vectorial/include/stats.h
@@ -32,20 +32,49 @@ extern "C" {
  *   Valor de retorno float: xmm0
  *
  *  F: En general, asigna registros por tipo de argumento, no por posición absoluta.
- * 
+ *
  * sum_array:
  *   rdi = arr, esi = n              -> retorna la suma en xmm0
+
+	rdi = arr: Recibe en el registro de 64 bits (rdi) el puntero o dirreccion base del arreglo de entrada
+	esi = n: Recibe en el registro de 32 bits (esi) el numero de elementos (n) que contiene el arreglo
+	Esta funcion debe retornar la suma en el registro xmm0, puesto que devuelve el resultado flotante acumulado en el
+	registro de punto flotante/SIMD llamado xmm0
+
  *
  * compute_stats:
  *   rdi = arr, esi = n, rdx = mean*, rcx = var*, r8 = min*, r9 = max*
  *   (var = varianza POBLACIONAL: var = sum((x - mean)^2) / n)
  *   Caso borde: si n == 0, escriba 0.0 en mean/var/min/max.
+
+	rdi = arr: Recibe en el registro de 64 bits (rdi) el puntero o dirreccion base del arreglo de entrada
+	esi = n: Recibe en el registro de 32 bits (esi) el numero de elementos (n) que contiene el arreglo
+
+	rdi = arr, esi = n, rdx = mean*, rcx = var*, r8 = min*, r9 = max
+	Punteros de memoria donde la subrutina debe escribir (por referencia) los resultados calculados: la media, la varianza
+	poblacional, el valor minimo y el valor maximo.
+
+	Asimismo, se analiza el caso borde: si n == 0, se debe escribir 0,0 en cada una de esas direcciones de memoria para
+	evitar fallos
  *
  * normalize_array:
  *   rdi = in, rsi = out, edx = n, xmm0 = mean, xmm1 = stddev
  *   out[i] = (in[i] - mean) / stddev
  *   Caso borde: si stddev == 0.0, copie in[i] en out[i] tal cual
  *   (evite division por cero).
+
+	rdi = in: Puntero al arreglo de entrada original
+	rsi = out:  Puntero al arreglo de salida donde se guardaran los datos normalizados.
+	edx =  n: Es el tamano del arreglo
+	xmm0 = mean: Valor de la media ya calculado que se pasa como un argumento de entrada en un valor tipo punto flotante.
+	xmm1 = stddev: Es el valor de la desviacion estandar de entrada
+
+	Formula: se aplica la normalizacion estadistica estandar la cual se calcula con
+	out [i] = ( in [i] - mean ) / stddev
+
+	Finalmente se debe analizar el caso borde que es cuando stddev == 0.0, esta copia directamente in [i] en out [i] para prevenir 
+	una interrupcion por division entre cero.
+
  */
 
 float sum_array(const float *arr, int n);
